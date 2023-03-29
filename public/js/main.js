832980cda278968415dc1d7d5be7d6c35cbf5129
Sreeharie123 <sreehariengoli@gmail.com>
2023-03-29 13:16:06 +0530

    current time function is added (moment.js)

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,50 @@
+const chatForm=document.getElementById('chat-form')
+const chatMessage=document.querySelector('.chat-messages')
+
 const socket= io()
+
+//Message from server
+socket.on('message',message=>{
+    console.log(message)
+    outputMessage(message)
+
+    //Scroll down
+
+    chatMessage.scrollTop=chatMessage.scrollHeight;
+})
+
+
+//Message submit
+chatForm.addEventListener('submit',(e)=>{
+
+    //prevent the default nature
+    e.preventDefault()
+
+    //Get the message text
+    const msg=e.target.elements.msg.value
+
+    //Emit message to server
+    socket.emit('chatMessage',msg)
+
+    //Clear input
+    e.target.elements.msg.value=''
+    e.target.elements.msg.focus();
+
+}) 
+
+
+//outputMessage to DOM
+
+function outputMessage(message){
+
+
+    const div=document.createElement('div');
+    div.classList.add('message');
+      div.innerHTML=`
+      <p class="meta">Mary <span>9:15pm</span></p>
+      <p class="text">
+         ${message}
+      </p>
+ `
+ document.querySelector('.chat-messages').appendChild(div);
+}
--- server.js
+++ server.js
@@ -2,18 +2,36 @@ const express=require('express');
 const path=require('path')
 const http=require('http')
 const socketio=require('socket.io')
+const formatMessage=require('./utils/messages')
 
-const app=express();
-const server=http.createServer(app) 
+const app=express(); 
+const server=http.createServer(app)  
 const io=socketio(server);
 
+
+const botName="chatAppBot"
 // Set static folder
 app.use(express.static(path.join(__dirname,'public')))
 
 //Run when client connects
 
 io.on('connection',socket=>{
-    console.log("new ws connection") 
+
+    //Welcome current User
+    socket.emit('message',formatMessage(botName,'Welcome to chatApp'))
+
+    //User connects broadcast  
+    socket.broadcast.emit('message',formatMessage(botName,'A user has joined the chat'))
+
+    //User disconnects the broadcast
+    socket.on('disconnect',()=>{
+        io.emit('message',formatMessage(botName,'A user has left the chat'))
+    })
+
+    //listen for chat message
+    socket.on('chatMessage',(msg)=>{
+        io.emit('message',formatMessage("USER",msg))
+    });
 })
 
 const PORT=3000||process.env.PORT;
@@ -21,3 +39,4 @@ const PORT=3000||process.env.PORT;
 server.listen(PORT,()=>{
     console.log("Server running on port :",PORT)
 }) 
+ 
